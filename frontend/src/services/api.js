fe903d24868d575d44566456702a7ce53c31c810
Sheikh Muhammad Khizar <shkkhizar27@gmail.com>
2025-10-25 17:23:33 +0500

    commit changes

--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/expenses";
-
-export const getExpenses = () => axios.get(API_URL);
-export const addExpense = (expense) => axios.post(API_URL, expense);
-export const deleteExpense = (id) => axios.delete(`${API_URL}/${id}`);
